524bff3fefa6167f3ad72bb1f03ecd76bcfd2fc5
GabrielGQA <gabrielgqa@gmail.com>
2020-01-15 11:06:42 -0300

    estilização sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 
-import './global.css'
+import './global.css';
+import './App.css';
+import './Sidebar.css';
 
 function App() {
   return (
     <div id="app">
       <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="git_username">Usuário do Github</label>
+            <input name="git_username" id="git_username" required/>
+          </div>
 
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+
+        </form>
       </aside>
       <main></main>
     </div>
